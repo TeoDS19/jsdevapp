5ab690b45e58d61b89d72b5bf671f18a2f6768b1
Frontend JavaScript <frontendjavascript@iMac-04.local>
2024-06-05 20:40:40 +0300

    curs3

---
+++ curs3/ex2_v3.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue;
+let vm = createApp({
+    data() {
+        return {
+            fname: "Ion",
+            lname: "Popescu",
+            address: "Brasov",
+            message: "Imi place JavaScript",
+           
+        }
+    },
+    methods: {
+        myName: function () {
+            return "Eu ma numesc " + this.fname + " " + this.lname + " si sunt din " + this.address;
+        }
+    }
+}).mount('#app');
+
+let app = createApp({
+    data() {
+        return {
+            seen: false,
+            limbaje: [
+                { id:10,text: 'JavaScript' },
+                { id :11,text: 'Java' },
+                { id:12,text: 'PHP' }
+            ],
+            htmlcontent: "<div><h3>Vue Js este interesant!</h3></div>"
+        }
+    }
+
+}).mount('#app2')
---
+++ curs3/ex3_v3.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue;
+let vm = createApp({
+    data() {
+        return {
+            num1: 300,
+            num2: 230,
+            num11:0,
+            num22:0,
+            total: '',
+            styleobj: {
+                width: "100px",
+                height: "100px",
+                backgroundColor: "red"
+            },
+            hreflink: "https://www.google.com"
+        }
+    },
+    methods: {
+        afiseazaTotal: function (event) {
+            console.log(event);
+            return this.total = this.num1 + this.num2;
+        },
+        afiseazaTotal2: function (a,b) {
+           this.num11=a;
+           this.num22=b;
+            return this.total1 = this.num11 + this.num22;
+        },
+        changebgcolor: function () {
+            this.styleobj.backgroundColor = "green";
+        },
+        originalcolor: function () {
+            this.styleobj.backgroundColor = "red";
+        }
+    },
+}).mount('#databinding');
